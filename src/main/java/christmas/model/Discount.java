9af69e99aaaf02822ac9366f0ac5f8e528200fa0
leeedohyun <ldh19738@gmail.com>
2023-11-10 22:19:47 +0900

    refactor: Money 클래스 적용

--- src/main/java/christmas/model/Discount.java
+++ src/main/java/christmas/model/Discount.java
@@ -4,36 +4,38 @@ public class Discount {
 
     private static final int D_DAY_BASIC_DISCOUNT_PRICE = 1000;
     private static final int D_DAY_ADDITIONAL_DISCOUNT_PRICE = 100;
-    private static final int MINIMUM_ORDER_PRICE = 10000;
     private static final int WEEKDAY_AND_WEEKEND_DISCOUNT_PRICE = 2023;
     private static final int SPECIAL_DISCOUNT_PRICE = 1000;
 
-    public int discountDDay(final Day visitedDay, final int totalPrice) {
-        if (visitedDay.isBeforeOrEqualChristmas() && totalPrice >= MINIMUM_ORDER_PRICE) {
-            return D_DAY_BASIC_DISCOUNT_PRICE
+    public Money discountDDay(final Day visitedDay, final Money totalPrice) {
+        if (visitedDay.isBeforeOrEqualChristmas() && totalPrice.isOverTenThousand()) {
+            int dDayDiscountPrice = D_DAY_BASIC_DISCOUNT_PRICE
                     + visitedDay.calculateDaysUntilEventStart() * D_DAY_ADDITIONAL_DISCOUNT_PRICE;
+            return new Money(dDayDiscountPrice);
         }
-        return 0;
+        return new Money(0);
     }
 
-    public int discountWeekDay(final Day visitedDay, final int totalPrice, final int numberOfDessertMenus) {
-        if (visitedDay.isEventNotEnded() && !visitedDay.isWeekend() && totalPrice >= MINIMUM_ORDER_PRICE) {
-            return WEEKDAY_AND_WEEKEND_DISCOUNT_PRICE * numberOfDessertMenus;
+    public Money discountWeekDay(final Day visitedDay, final Money totalPrice, final int numberOfDessertMenus) {
+        if (visitedDay.isEventNotEnded() && !visitedDay.isWeekend() && totalPrice.isOverTenThousand()) {
+            int weekDayDiscountPrice = WEEKDAY_AND_WEEKEND_DISCOUNT_PRICE * numberOfDessertMenus;
+            return new Money(weekDayDiscountPrice);
         }
-        return 0;
+        return new Money(0);
     }
 
-    public int discountWeekend(final Day visitedDay, final int totalPrice, final int numberOfMainMenu) {
-        if (visitedDay.isEventNotEnded() && visitedDay.isWeekend() && totalPrice >= MINIMUM_ORDER_PRICE) {
-            return WEEKDAY_AND_WEEKEND_DISCOUNT_PRICE * numberOfMainMenu;
+    public Money discountWeekend(final Day visitedDay, final Money totalPrice, final int numberOfMainMenu) {
+        if (visitedDay.isEventNotEnded() && visitedDay.isWeekend() && totalPrice.isOverTenThousand()) {
+            int weekendDiscountPrice = WEEKDAY_AND_WEEKEND_DISCOUNT_PRICE * numberOfMainMenu;
+            return new Money(weekendDiscountPrice);
         }
-        return 0;
+        return new Money(0);
     }
 
-    public int discountSpecial(final Day visitedDay, final int totalPrice) {
-        if (visitedDay.isEventNotEnded() && visitedDay.hasStar() && totalPrice >= MINIMUM_ORDER_PRICE) {
-            return SPECIAL_DISCOUNT_PRICE;
+    public Money discountSpecial(final Day visitedDay, final Money totalPrice) {
+        if (visitedDay.isEventNotEnded() && visitedDay.hasStar() && totalPrice.isOverTenThousand()) {
+            return new Money(SPECIAL_DISCOUNT_PRICE);
         }
-        return 0;
+        return new Money(0);
     }
 }
--- src/main/java/christmas/model/Money.java
+++ src/main/java/christmas/model/Money.java
@@ -1,5 +1,7 @@
 package christmas.model;
 
+import java.util.Objects;
+
 public class Money {
 
     private static final int MINIMUM_ORDER_PRICE = 10000;
@@ -10,7 +12,23 @@ public class Money {
         this.money = money;
     }
 
-    public boolean isTotalPriceOverTenThousand() {
+    public boolean isOverTenThousand() {
         return money >= MINIMUM_ORDER_PRICE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Money money1)) {
+            return false;
+        }
+        return money == money1.money;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(money);
     }
 }
