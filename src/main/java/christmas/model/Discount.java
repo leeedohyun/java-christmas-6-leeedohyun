253b8d553463b7d85bd8e0551554dba81eb8cc1c
leeedohyun <ldh19738@gmail.com>
2023-11-10 17:13:52 +0900

    feat: 평일 할인 금액 계산하는 기능 구현

--- src/main/java/christmas/model/Discount.java
+++ src/main/java/christmas/model/Discount.java
@@ -2,14 +2,24 @@ package christmas.model;
 
 public class Discount {
 
-    private static final int FIRST_DAY = 1;
+    private static final int EVENT_START_DAY = 1;
     private static final int CHRISTMAS = 25;
-    private static final int BASIC_DISCOUNT_PRICE = 1000;
-    private static final int ADDITIONAL_DISCOUNT_PRICE = 100;
+    private static final int D_DAY_BASIC_DISCOUNT_PRICE = 1000;
+    private static final int D_DAY_ADDITIONAL_DISCOUNT_PRICE = 100;
+    private static final int EVENT_END_DATE = 31;
+    private static final int MINIMUM_ORDER_PRICE = 10000;
+    private static final int WEEKDAY_AND_WEEKEND_DISCOUNT_PRICE = 2023;
 
     public int discountDDay(int visitedDay, int totalPrice) {
-        if (visitedDay <= CHRISTMAS && totalPrice >= 10000) {
-            return BASIC_DISCOUNT_PRICE + (visitedDay - FIRST_DAY) * ADDITIONAL_DISCOUNT_PRICE;
+        if (visitedDay <= CHRISTMAS && totalPrice >= MINIMUM_ORDER_PRICE) {
+            return D_DAY_BASIC_DISCOUNT_PRICE + (visitedDay - EVENT_START_DAY) * D_DAY_ADDITIONAL_DISCOUNT_PRICE;
+        }
+        return 0;
+    }
+
+    public int discountWeekDay(int visitedDay, int totalPrice, int numberOfDessertMenus) {
+        if (visitedDay <= EVENT_END_DATE && totalPrice >= MINIMUM_ORDER_PRICE) {
+            return WEEKDAY_AND_WEEKEND_DISCOUNT_PRICE * numberOfDessertMenus;
         }
         return 0;
     }
