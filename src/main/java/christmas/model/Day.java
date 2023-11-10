bd7872511926ecea7866fb6fc84ba21bb0d30ef8
leeedohyun <ldh19738@gmail.com>
2023-11-10 18:03:24 +0900

    feat: 이벤트 이전인지 확인하는 기능 구현

--- src/main/java/christmas/model/Day.java
+++ src/main/java/christmas/model/Day.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 public class Day {
 
     private static final LocalDate CHRISTMAS = LocalDate.of(2023, 12, 25);
+    private static final LocalDate EVENT_END_DATE = LocalDate.of(2023, 12, 31);
 
     private final LocalDate day;
 
@@ -19,5 +20,9 @@ public class Day {
 
     public boolean isBeforeOrEqualChristmas() {
         return day.isBefore(CHRISTMAS) || day.isEqual(CHRISTMAS);
+    }
+
+    public boolean isEventNotEnded() {
+        return day.isBefore(EVENT_END_DATE) || day.isEqual(EVENT_END_DATE);
     }
 }
