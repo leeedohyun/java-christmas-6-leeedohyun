576aba475a54b7caaacf495cddbcb3cb22ae1ea7
leeedohyun <ldh19738@gmail.com>
2023-11-11 15:47:23 +0900

    refactor: 반환 타입 int -> LocalDate로 변경

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,12 +2,13 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.model.Utils;
+import java.time.LocalDate;
 import java.util.List;
 
 public class InputView {
 
-    public int inputVisitedDate() {
-        return Utils.convertStringToInt(Console.readLine());
+    public LocalDate inputVisitedDate() {
+        return Utils.convertStringToLocalDate(Console.readLine());
     }
 
     public List<String> inputMenusWithQuantity() {
