77837d9f9a890d1509a11c15e55d935042a07dab
leeedohyun <ldh19738@gmail.com>
2023-11-11 12:59:34 +0900

    feat: 메뉴와 개수 입력하는 기능 구현

--- src/main/java/christmas/model/Utils.java
+++ src/main/java/christmas/model/Utils.java
@@ -1,17 +1,26 @@
 package christmas.model;
 
+import java.util.Arrays;
+import java.util.List;
+
 public final class Utils {
 
     private static final String DATE_FORMAT_EXCEPTION_MESSAGE = "[ERROR] 날짜는 숫자만 입력할 수 있습니다. 다시 입력해 주세요.";
+    private static final String MENU_SEPARATOR = ",";
 
     private Utils() {
     }
 
-    public static int convertStringToInt(String string) {
+    public static int convertStringToInt(final String string) {
         try {
             return Integer.parseInt(string);
         } catch (NumberFormatException numberFormatException) {
             throw new IllegalArgumentException(DATE_FORMAT_EXCEPTION_MESSAGE);
         }
+    }
+
+    public static List<String> splitDifferentMenus(final String string) {
+        return Arrays.stream(string.split(MENU_SEPARATOR))
+                .toList();
     }
 }
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,10 +2,15 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.model.Utils;
+import java.util.List;
 
 public class InputView {
 
     public int inputVisitedDate() {
         return Utils.convertStringToInt(Console.readLine());
+    }
+
+    public List<String> inputMenusWithQuantity() {
+        return Utils.splitDifferentMenus(Console.readLine());
     }
 }
