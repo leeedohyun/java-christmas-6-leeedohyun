e39fdfa3b4f4b318b637d65ea4ee9a7806029fde
leeedohyun <ldh19738@gmail.com>
2023-11-11 11:27:21 +0900

    test: 메인 메뉴 개수 세는 기능 테스트

--- src/test/java/christmas/model/OrderTest.java
+++ src/test/java/christmas/model/OrderTest.java
@@ -1,5 +1,6 @@
 package christmas.model;
 
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +18,18 @@ class OrderTest {
 
         // then
         Assertions.assertEquals(discountedPrice, 9_000);
+    }
+
+    @Test
+    public void 주문_내역에서_메인_메뉴_개수_테스트() {
+        // given
+        final List<Menu> orderedMenu = List.of(Menu.CHAMPAGNE, Menu.CHRISTMAS_PASTA, Menu.T_BONE_STEAK, Menu.ICE_CREAM);
+
+        // when
+        final Order order = new Order();
+        final int countedMainMenu = order.countMainMenu(orderedMenu);
+
+        // then
+        Assertions.assertEquals(countedMainMenu, 2);
     }
 }
