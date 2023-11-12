40ca34656af1074ab0da6a4dd2da2fa2ce3cf907
leeedohyun <ldh19738@gmail.com>
2023-11-12 12:48:17 +0900

    refactor: 출력 메시지 OuputView에 정의

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -11,7 +11,6 @@ import christmas.model.Order;
 import christmas.model.Utils;
 import christmas.view.InputView;
 import christmas.view.OutputView;
-import java.util.List;
 import java.util.Map;
 
 public class ChristmasController {
@@ -26,70 +25,63 @@ public class ChristmasController {
 
     public void play() {
         outputView.printWelcomeMessage();
-        while (true) {
-            try {
-                Day day = createValidDate();
+        try {
+            Day day = createValidDate();
 
-                Order order = createValidOrder();
+            Order order = createValidOrder();
 
-                outputView.printEventPreview(day);
-                outputView.printOrderedMenus(order);
+            outputView.printEventPreview(day);
+            outputView.printOrderedMenus(order);
 
-                Money total = order.calculateOrderedPriceBeforeDiscount();
-                outputView.printPriceBeforeDiscount(total);
+            Money total = order.calculateOrderedPriceBeforeDiscount();
+            outputView.printPriceBeforeDiscount(total);
 
-                System.out.println("<증정 메뉴>");
-                GiveawayEvent giveawayEvent = GiveawayEvent.create(total);
-                System.out.println(giveawayEvent.getGiveawayMenu());
+            System.out.println("<증정 메뉴>");
+            GiveawayEvent giveawayEvent = GiveawayEvent.create(total);
+            System.out.println(giveawayEvent.getGiveawayMenu());
 
-                Discount discount = new Discount();
+            Discount discount = new Discount();
 
-                Money dDay = discount.discountDDay(day, total);
-                Money weekDay = discount.discountWeekDay(day, total, order.countDessert());
-                Money weekend = discount.discountWeekend(day, total, order.countMainMenu());
-                Money special = discount.discountSpecial(day, total);
+            Money dDay = discount.discountDDay(day, total);
+            Money weekDay = discount.discountWeekDay(day, total, order.countDessert());
+            Money weekend = discount.discountWeekend(day, total, order.countMainMenu());
+            Money special = discount.discountSpecial(day, total);
 
-                System.out.println();
-                System.out.println("<혜택 내역>");
-                Money event = giveawayEvent.getGiveawayMenuPrice();
-                if (!dDay.equals(Constants.ZERO_WON)) {
-                    System.out.printf("크리스마스 디데이 할인: -%s원\n", dDay.getFormattedMoney());
-                }
-                if (!weekDay.equals(Constants.ZERO_WON)) {
-                    System.out.printf("평일 할인: -%s원\n", weekDay.getFormattedMoney());
-                }
-                if (!weekend.equals(Constants.ZERO_WON)) {
-                    System.out.printf("주말 할인: -%s원\n", weekend.getFormattedMoney());
-                }
-                if (!special.equals(Constants.ZERO_WON)) {
-                    System.out.printf("특별 할인: -%s원\n", special.getFormattedMoney());
-                }
-                if (giveawayEvent.isEventActive()) {
-                    System.out.printf("증정 이벤트: -%s원\n", event.getFormattedMoney());
-                    System.out.println();
-                }
-                if (dDay.equals(Constants.ZERO_WON) && weekDay.equals(Constants.ZERO_WON) && weekend.equals(
-                        Constants.ZERO_WON)
-                        && special.equals(Constants.ZERO_WON) && !giveawayEvent.isEventActive()) {
-                    System.out.println("없음\n");
-                }
-
-                System.out.println("<총혜택 금액>");
-                Money discountPrice = dDay.plus(weekDay.plus(weekend.plus(special.plus(event))));
-                System.out.printf("-%s원\n", discountPrice.getFormattedMoney());
-                System.out.println();
-
-                System.out.println("<할인 후 예상 결제 금액>");
-                System.out.printf("%s원\n", total.plus(discountPrice.multiply(-1).plus(event)).getFormattedMoney());
-                System.out.println();
-
-                System.out.println("<12월 이벤트 배지>");
-                Badge badge = Badge.decide(discountPrice);
-                System.out.println(badge.getName());
-                break;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
+            System.out.println();
+            System.out.println("<혜택 내역>");
+            Money event = giveawayEvent.getGiveawayMenuPrice();
+            if (!dDay.equals(Constants.ZERO_WON)) {
+                System.out.printf("크리스마스 디데이 할인: -%s원\n", dDay.getFormattedMoney());
             }
+            if (!weekDay.equals(Constants.ZERO_WON)) {
+                System.out.printf("평일 할인: -%s원\n", weekDay.getFormattedMoney());
+            }
+            if (!weekend.equals(Constants.ZERO_WON)) {
+                System.out.printf("주말 할인: -%s원\n", weekend.getFormattedMoney());
+            }
+            if (!special.equals(Constants.ZERO_WON)) {
+                System.out.printf("특별 할인: -%s원\n", special.getFormattedMoney());
+            }
+            if (giveawayEvent.isEventActive()) {
+                System.out.printf("증정 이벤트: -%s원\n", event.getFormattedMoney());
+                System.out.println();
+            }
+            if (dDay.equals(Constants.ZERO_WON) && weekDay.equals(Constants.ZERO_WON) && weekend.equals(
+                    Constants.ZERO_WON)
+                    && special.equals(Constants.ZERO_WON) && !giveawayEvent.isEventActive()) {
+                System.out.println("없음\n");
+            }
+
+            System.out.println();
+            Money discountPrice = dDay.plus(weekDay.plus(weekend.plus(special.plus(event))));
+            outputView.printTotalBenefitPrice(discountPrice);
+
+            outputView.printDiscountedPrice(total.plus(discountPrice.multiply(-1).plus(event)));
+
+            Badge badge = Badge.decide(discountPrice);
+            outputView.printBadge(badge);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
     }
 
@@ -106,8 +98,7 @@ public class ChristmasController {
     private Map<Menu, Integer> createValidMenus() {
         try {
             outputView.printOrderInstruction();
-            List<String> strings = inputView.inputMenusWithQuantity();
-            return Utils.convertToMenuQuantityMap(strings);
+            return Utils.convertToMenuQuantityMap(inputView.inputMenusWithQuantity());
         } catch (IllegalStateException illegalStateException) {
             throw new IllegalStateException(Constants.MENU_NOT_FOUND_EXCEPTION_MESSAGE);
         }
@@ -119,14 +110,6 @@ public class ChristmasController {
         } catch (final IllegalArgumentException | IllegalStateException exception) {
             System.out.println(exception.getMessage());
             return createValidOrder();
-        }
-    }
-
-    private void validateDuplicateMenu(List<String> menuAndQuantity, Map<Menu, Integer> orderedMenus) {
-        System.out.println("menuAndQuantity = " + menuAndQuantity.size());
-        System.out.println("orderedMenus = " + orderedMenus.size());
-        if (menuAndQuantity.size() != orderedMenus.size()) {
-            throw new IllegalArgumentException(Constants.MENU_NOT_FOUND_EXCEPTION_MESSAGE);
         }
     }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,6 @@
 package christmas.view;
 
+import christmas.model.Badge;
 import christmas.model.Day;
 import christmas.model.Money;
 import christmas.model.Order;
@@ -9,11 +10,14 @@ public class OutputView {
     private static final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String VISIT_DATE_QUESTION_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String ORDER_INSTRUCTION_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
-    private static final String EVENT_PREVIEW_MESSAGE_FORMAT = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private static final String EVENT_PREVIEW_MESSAGE_FORMAT = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
     private static final String ORDER_MENU_HEADER = "<주문 메뉴>";
-    private static final String ORDER_MENU_FORMAT = "%s %d개";
+    private static final String ORDER_MENU_FORMAT = "%s %d개\n";
     private static final String PRICE_BEFORE_DISCOUNT_HEADER = "<할인 전 총주문 금액>";
-    private static final String PRICE_FORMAT = "%s원";
+    private static final String PRICE_FORMAT = "%s원\n";
+    public static final String TOTAL_BENEFIT_PRICE_HEADER = "<총혜택 금액>";
+    private static final String DISCOUNTED_PRICE_HEADER = "<할인 후 예상 결제 금액>";
+    private static final String BADGE_HEADER = "<12월 이벤트 배지>";
 
     public void printWelcomeMessage() {
         System.out.println(WELCOME_MESSAGE);
@@ -27,24 +31,39 @@ public class OutputView {
         System.out.println(ORDER_INSTRUCTION_MESSAGE);
     }
 
-    public void printEventPreview(Day visitedDay) {
+    public void printEventPreview(final Day visitedDay) {
         System.out.printf(EVENT_PREVIEW_MESSAGE_FORMAT, visitedDay.getMonth(), visitedDay.getDay());
-        printEmptyLine();
         printEmptyLine();
     }
 
-    public void printOrderedMenus(Order order) {
+    public void printOrderedMenus(final Order order) {
         System.out.println(ORDER_MENU_HEADER);
         order.getOrderedMenus()
                 .forEach((menu, quantity) -> System.out.printf(ORDER_MENU_FORMAT, menu.getName(), quantity));
         printEmptyLine();
-        printEmptyLine();
     }
 
-    public void printPriceBeforeDiscount(Money priceBeforeDiscount) {
+    public void printPriceBeforeDiscount(final Money priceBeforeDiscount) {
         System.out.println(PRICE_BEFORE_DISCOUNT_HEADER);
         System.out.printf(PRICE_FORMAT, priceBeforeDiscount.getFormattedMoney());
         printEmptyLine();
+    }
+
+    public void printTotalBenefitPrice(final Money totalBenefitPrice) {
+        System.out.println(TOTAL_BENEFIT_PRICE_HEADER);
+        System.out.printf(PRICE_FORMAT, totalBenefitPrice.getFormattedMoney());
+        printEmptyLine();
+    }
+
+    public void printDiscountedPrice(final Money discountedPrice) {
+        System.out.println(DISCOUNTED_PRICE_HEADER);
+        System.out.printf(PRICE_FORMAT, discountedPrice.getFormattedMoney());
+        printEmptyLine();
+    }
+
+    public void printBadge(final Badge badge) {
+        System.out.println(BADGE_HEADER);
+        System.out.println(badge.getName());
     }
 
     private void printEmptyLine() {
