a746d9878616a3ff2853e92707760d56288d31da
leeedohyun <ldh19738@gmail.com>
2023-11-13 23:49:41 +0900

    test: @BeforeEach 사용해서 코드 중복 삭제

--- src/test/java/christmas/model/OrderTest.java
+++ src/test/java/christmas/model/OrderTest.java
@@ -4,22 +4,27 @@ import christmas.model.discount.DiscountManager;
 import java.time.LocalDate;
 import java.util.Map;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class OrderTest {
 
-    @Test
-    public void 전체_할인_금액_계산_테스트() {
-        // given
+    private Order order;
+
+    @BeforeEach
+    void beforeEach() {
         final Day day = new Day(LocalDate.of(2023, 12, 3));
         final OrderDetail detail = new OrderDetail(
                 Map.of(Menu.T_BONE_STEAK, 1, Menu.BBQ_RIBS, 1, Menu.CHOCOLATE_CAKE, 2, Menu.ZERO_COLA, 1));
         final Money priceBeforeDiscount = new Money(142_000);
         final DiscountManager discountManager = new DiscountManager();
         final GiveawayEvent giveawayEvent = GiveawayEvent.create(priceBeforeDiscount);
+        order = new Order(day, detail, priceBeforeDiscount, discountManager, giveawayEvent);
+    }
 
+    @Test
+    public void 전체_할인_금액_계산_테스트() {
         // when
-        final Order order = new Order(day, detail, priceBeforeDiscount, discountManager, giveawayEvent);
         final Money discountPrice = order.calculateTotalDiscountPrice();
 
         //then
@@ -29,16 +34,9 @@ class OrderTest {
     @Test
     public void 할인된_결제_금액_계산_테스트() {
         // given
-        final Day day = new Day(LocalDate.of(2023, 12, 3));
-        final OrderDetail detail = new OrderDetail(
-                Map.of(Menu.T_BONE_STEAK, 1, Menu.BBQ_RIBS, 1, Menu.CHOCOLATE_CAKE, 2, Menu.ZERO_COLA, 1));
-        final Money priceBeforeDiscount = new Money(142_000);
-        final DiscountManager discountManager = new DiscountManager();
-        final GiveawayEvent giveawayEvent = GiveawayEvent.create(priceBeforeDiscount);
+        final Money discountPrice = order.calculateTotalDiscountPrice();
 
         // when
-        final Order order = new Order(day, detail, priceBeforeDiscount, discountManager, giveawayEvent);
-        final Money discountPrice = order.calculateTotalDiscountPrice();
         final Money discountedPrice = order.calculateDiscountedPrice(discountPrice);
 
         //then
