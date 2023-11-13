24b4c5422e99087af5b62d491717cc13f955dc71
leeedohyun <ldh19738@gmail.com>
2023-11-13 23:35:29 +0900

    test: 전체 할인 금액 계산 테스트

---
+++ src/test/java/christmas/model/OrderTest.java
@@ -0,0 +1,28 @@
+package christmas.model;
+
+import christmas.model.discount.DiscountManager;
+import java.time.LocalDate;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrderTest {
+
+    @Test
+    public void 전체_할인_금액_계산_테스트() {
+        // given
+        final Day day = new Day(LocalDate.of(2023, 12, 3));
+        final OrderDetail detail = new OrderDetail(
+                Map.of(Menu.T_BONE_STEAK, 1, Menu.BBQ_RIBS, 1, Menu.CHOCOLATE_CAKE, 2, Menu.ZERO_COLA, 1));
+        final Money priceBeforeDiscount = new Money(142_000);
+        final DiscountManager discountManager = new DiscountManager();
+        final GiveawayEvent giveawayEvent = GiveawayEvent.create(priceBeforeDiscount);
+
+        // when
+        final Order order = new Order(day, detail, priceBeforeDiscount, discountManager, giveawayEvent);
+        final Money discountPrice = order.calculateTotalDiscountPrice();
+
+        //then
+        Assertions.assertEquals(discountPrice, new Money(6_246));
+    }
+}
