921936e63f43fe80e86e59eca99b50fcbb426ee0
leeedohyun <ldh19738@gmail.com>
2023-11-13 23:38:26 +0900

    test: 할인된 금액 계산 테스트

--- src/test/java/christmas/model/OrderTest.java
+++ src/test/java/christmas/model/OrderTest.java
@@ -25,4 +25,23 @@ class OrderTest {
         //then
         Assertions.assertEquals(discountPrice, new Money(6_246));
     }
+
+    @Test
+    public void 할인된_결제_금액_계산_테스트() {
+        // given
+        final Day day = new Day(LocalDate.of(2023, 12, 3));
+        final OrderDetail detail = new OrderDetail(
+                Map.of(Menu.T_BONE_STEAK, 1, Menu.BBQ_RIBS, 1, Menu.CHOCOLATE_CAKE, 2, Menu.ZERO_COLA, 1));
+        final Money priceBeforeDiscount = new Money(142_000);
+        final DiscountManager discountManager = new DiscountManager();
+        final GiveawayEvent giveawayEvent = GiveawayEvent.create(priceBeforeDiscount);
+
+        // when
+        final Order order = new Order(day, detail, priceBeforeDiscount, discountManager, giveawayEvent);
+        final Money discountPrice = order.calculateTotalDiscountPrice();
+        final Money discountedPrice = order.calculateDiscountedPrice(discountPrice);
+
+        //then
+        Assertions.assertEquals(discountedPrice, new Money(135_754));
+    }
 }
