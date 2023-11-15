8dfe1e6d50b7018a1e51644114a8f484169b722c
leeedohyun <ldh19738@gmail.com>
2023-11-15 15:06:08 +0900

    refactor: 금액에서 빼기 연산을 수행할 때 결과가 음수인 경우 예외 처리

--- src/main/java/christmas/model/Price.java
+++ src/main/java/christmas/model/Price.java
@@ -6,36 +6,41 @@ import java.util.Objects;
 public class Price {
 
     private static final String MONEY_FORMAT_PATTERN = "###,###";
+    public static final String INVALID_PRICE_EXCEPTION = "[ERROR] 가격은 음수일 수 없습니다.";
 
-    private final int money;
+    private final int price;
 
-    public Price(final int money) {
-        this.money = money;
+    public Price(final int price) {
+        this.price = price;
     }
 
     public Price plus(final Price another) {
-        return new Price(money + another.money);
+        return new Price(price + another.price);
     }
 
     public Price multiply(final int numberOfMenu) {
-        return new Price(money * numberOfMenu);
+        return new Price(price * numberOfMenu);
     }
 
     public Price minus(final Price another) {
-        return new Price(money - another.money);
+        int minussedPrice = price - another.price;
+        if (minussedPrice < 0) {
+            throw new IllegalArgumentException(INVALID_PRICE_EXCEPTION);
+        }
+        return new Price(minussedPrice);
     }
 
     public boolean isEqualAndOver(final Price another) {
-        return money >= another.money;
+        return price >= another.price;
     }
 
     public boolean isUnder(final Price another) {
-        return money < another.money;
+        return price < another.price;
     }
 
     public String getFormattedMoney() {
         DecimalFormat decimalFormat = new DecimalFormat(MONEY_FORMAT_PATTERN);
-        return decimalFormat.format(money);
+        return decimalFormat.format(price);
     }
 
     @Override
@@ -43,14 +48,15 @@ public class Price {
         if (this == o) {
             return true;
         }
-        if (!(o instanceof Price price1)) {
+        if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        return money == price1.money;
+        Price price1 = (Price) o;
+        return price == price1.price;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(money);
+        return Objects.hash(price);
     }
 }
