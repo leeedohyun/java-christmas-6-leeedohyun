56059e029a38edbd43c41ce50a2f24c231afada5
leeedohyun <ldh19738@gmail.com>
2023-12-27 00:10:47 +0900

    refactor: ViewConstants 제거

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -8,12 +8,12 @@ import christmas.util.Utils;
 public class InputView {
 
     public int inputDateOfVisit() {
-        System.out.println(ViewConstants.VISIT_DATE_QUESTION_MESSAGE);
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         return Utils.convertStringToLocalDate(Console.readLine());
     }
 
     public List<String> inputMenusWithQuantity() {
-        System.out.println(ViewConstants.ORDER_INSTRUCTION_MESSAGE);
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         return Utils.splitDifferentMenus(Console.readLine());
     }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -12,11 +12,16 @@ import christmas.util.ViewFormatter;
 
 public class OutputView {
 
-    private static final String GIVEAWAY_EVENT_MENU = "샴페인 1개";
     private static final String NO_BENEFIT_MESSAGE = "없음";
+    private static final List<String> BENEFIT_DETAIL_MESSAGES = List.of("크리스마스 디데이 할인", "평일 할인", "주말 할인", "특별 할인");
+
+    private static final int CHRISTMAS_DAY_DISCOUNT_INDEX = 0;
+    private static final int WEEKDAY_DISCOUNT_INDEX = 1;
+    private static final int WEEKEND_DISCOUNT_INDEX = 2;
+    private static final int SPECIAL_DISCOUNT_INDEX = 3;
 
     public void printWelcomeMessage() {
-        System.out.println(ViewConstants.WELCOME_MESSAGE);
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
 
     public void printOrderDetails(final Date dateOfVisit, final OrderDetail orderDetail) {
@@ -25,8 +30,8 @@ public class OutputView {
     }
 
     public void printPriceBeforeDiscount(final Price priceBeforeDiscount) {
-        System.out.println(ViewConstants.PRICE_BEFORE_DISCOUNT_HEADER);
-        System.out.printf(ViewConstants.PRICE_FORMAT, ViewFormatter.getFormattedMoney(priceBeforeDiscount));
+        System.out.println("<할인 전 총주문 금액>");
+        System.out.printf("%s원\n", ViewFormatter.getFormattedMoney(priceBeforeDiscount));
         printEmptyLine();
     }
 
@@ -38,13 +43,13 @@ public class OutputView {
     }
 
     public static void printExceptionMessage(final Exception exception) {
-        System.out.println(ViewConstants.ERROR_HEADER + exception.getMessage());
+        System.out.println("[ERROR] " + exception.getMessage());
     }
 
     public void printGiveawayMenu(final GiveawayEvent giveawayEvent) {
-        System.out.println(ViewConstants.DEFAULT_GIVEAWAY_MENU_HEADER);
+        System.out.println("<증정 메뉴>");
         if (giveawayEvent.canGetGiveawayEventMenu()) {
-            System.out.println(GIVEAWAY_EVENT_MENU);
+            System.out.println("샴페인 1개");
             return;
         }
         System.out.println(NO_BENEFIT_MESSAGE);
@@ -52,16 +57,16 @@ public class OutputView {
     }
 
     public void printDiscountBenefitDetails(final List<Price> discountPrices, final Price event) {
-        System.out.println(ViewConstants.BENEFIT_HEADER);
-        printBenefitDetails(ViewConstants.BENEFIT_DETAIL_MESSAGES.get(ViewConstants.CHRISTMAS_DAY_DISCOUNT_INDEX),
-                discountPrices.get(ViewConstants.CHRISTMAS_DAY_DISCOUNT_INDEX));
-        printBenefitDetails(ViewConstants.BENEFIT_DETAIL_MESSAGES.get(ViewConstants.WEEKDAY_DISCOUNT_INDEX),
-                discountPrices.get(ViewConstants.WEEKDAY_DISCOUNT_INDEX));
-        printBenefitDetails(ViewConstants.BENEFIT_DETAIL_MESSAGES.get(ViewConstants.WEEKEND_DISCOUNT_INDEX),
-                discountPrices.get(ViewConstants.WEEKEND_DISCOUNT_INDEX));
-        printBenefitDetails(ViewConstants.BENEFIT_DETAIL_MESSAGES.get(ViewConstants.SPECIAL_DISCOUNT_INDEX),
-                discountPrices.get(ViewConstants.SPECIAL_DISCOUNT_INDEX));
-        printBenefitDetails(ViewConstants.GIVEAWAY_EVENT_MESSAGE, event);
+        System.out.println("<혜택 내역>");
+        printBenefitDetails(BENEFIT_DETAIL_MESSAGES.get(CHRISTMAS_DAY_DISCOUNT_INDEX),
+                discountPrices.get(CHRISTMAS_DAY_DISCOUNT_INDEX));
+        printBenefitDetails(BENEFIT_DETAIL_MESSAGES.get(WEEKDAY_DISCOUNT_INDEX),
+                discountPrices.get(WEEKDAY_DISCOUNT_INDEX));
+        printBenefitDetails(BENEFIT_DETAIL_MESSAGES.get(WEEKEND_DISCOUNT_INDEX),
+                discountPrices.get(WEEKEND_DISCOUNT_INDEX));
+        printBenefitDetails(BENEFIT_DETAIL_MESSAGES.get(SPECIAL_DISCOUNT_INDEX),
+                discountPrices.get(SPECIAL_DISCOUNT_INDEX));
+        printBenefitDetails("증정 이벤트", event);
     }
 
     public void printNoBenefitIfApplicable(final Price totalBenefitPrice) {
@@ -76,38 +81,38 @@ public class OutputView {
     }
 
     private void printEventPreview(final Date dateOfVisit) {
-        System.out.printf(ViewConstants.EVENT_PREVIEW_MESSAGE_FORMAT, dateOfVisit.getMonth(), dateOfVisit.getDate());
+        System.out.printf("%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n", dateOfVisit.getMonth(), dateOfVisit.getDate());
         printEmptyLine();
     }
 
     private void printOrderedMenus(final OrderDetail orderDetail) {
-        System.out.println(ViewConstants.ORDER_MENU_HEADER);
+        System.out.println("<주문 메뉴>");
         orderDetail.getOrderedMenus()
                 .forEach((menu, quantity) ->
-                        System.out.printf(ViewConstants.ORDER_MENU_FORMAT, menu.getName(), quantity));
+                        System.out.printf("%s %d개\n", menu.getName(), quantity));
         printEmptyLine();
     }
 
     private void printTotalBenefitPrice(final Price totalBenefitPrice) {
-        System.out.println(ViewConstants.TOTAL_BENEFIT_PRICE_HEADER);
-        System.out.printf(ViewConstants.DISCOUNT_PRICE_FORMAT, ViewFormatter.getFormattedMoney(totalBenefitPrice));
+        System.out.println("<총혜택 금액>");
+        System.out.printf("-%s원\n", ViewFormatter.getFormattedMoney(totalBenefitPrice));
         printEmptyLine();
     }
 
     private void printDiscountedPrice(final Price discountedPrice) {
-        System.out.println(ViewConstants.DISCOUNTED_PRICE_HEADER);
-        System.out.printf(ViewConstants.PRICE_FORMAT, ViewFormatter.getFormattedMoney(discountedPrice));
+        System.out.println("<할인 후 예상 결제 금액>");
+        System.out.printf("%s원\n", ViewFormatter.getFormattedMoney(discountedPrice));
         printEmptyLine();
     }
 
     private void printBadge(final Badge badge) {
-        System.out.println(ViewConstants.BADGE_HEADER);
+        System.out.println("<12월 이벤트 배지>");
         System.out.println(badge.getName());
     }
 
     private void printBenefitDetails(final String message, final Price discountPrice) {
         if (!discountPrice.isZero()) {
-            System.out.printf(ViewConstants.BENEFIT_DETAIL_FORMAT, message, ViewFormatter.getFormattedMoney(discountPrice));
+            System.out.printf("%s: -%s원\n", message, ViewFormatter.getFormattedMoney(discountPrice));
         }
     }
 }
